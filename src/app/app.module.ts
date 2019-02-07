919a9252f46af355c8567bf0b17c541dfb99b6b0
RubenCardozo <RubenCardozo@users.noreply.github.com>
2019-02-07 15:57:20 +0100

    Resume, Contac, Portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BackcoverComponent } from './pages/backcover/backcover.component';
+import { CoverComponent } from './pages/cover/cover.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    ContactComponent,
+    BackcoverComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/backcover/backcover.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-backcover',
+  templateUrl: './backcover.component.html',
+  styleUrls: ['./backcover.component.css']
+})
+export class BackcoverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
